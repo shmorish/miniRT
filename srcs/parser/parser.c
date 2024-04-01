bba141a7ecdfdaa0559e9a14e761b9f33c8ff3f1
Ishi <syi378039@gmail.com>
2024-04-02 08:55:42 +0900

    全部セットされているか確認

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:28:54 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/02 08:38:26 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/02 08:55:23 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@ static void	is_set_init(t_is_set *is_set)
 	is_set->light = false;
 }
 
+static check_is_set(t_is_set is_set)
+{
+	if (is_set.ambient_lightning == false
+		|| is_set.camera == false
+		|| is_set.light == false)
+		print_error_and_exit("ambient lightning, camera, light, must be set");
+}
+
 void	parser(t_data *data, int argc, char **argv)
 {
 	is_set_init(&data->is_set);
@@ -26,4 +34,5 @@ void	parser(t_data *data, int argc, char **argv)
 	check_argument(argc, argv);
 	check_and_exit_if_directory(argv[1]);
 	input_file(data, argv[1]);
+	check_is_set(is_set);
 }
