fb4b8e908879acb5c12fd81f828820975c279544
Ishi <syi378039@gmail.com>
2024-04-01 21:17:12 +0900

    fix check_argument

--- srcs/parser/check_argument.c
+++ srcs/parser/check_argument.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:40:34 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/01 21:10:02 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/01 21:16:49 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ static bool	check_argument_count(int argc)
 
 static bool	check_argument_file_name(char *argv)
 {
-	if (!ft_strrchr(argv, '.'))
+	size_t	len;
+
+	len = ft_strlen(argv);
+	if (len <= 3)
 		return (false);
-	if (ft_strncmp(ft_strrchr(argv, '.'), ".rt", 3))
-		return (false);
-	if (strcmp(argv, ".rt") == 0)
+	if (ft_strncmp(&argv[len - 3], ".rt", 3))
 		return (false);
 	return (true);
 }
@@ -37,4 +38,3 @@ void	check_argument(int argc, char **argv)
 	if (!check_argument_file_name(argv[1]))
 		print_error_and_exit(ERR_ARGV);
 }
-
