876badee1f4d0b80b6ee5ce28827d5c0ebb1cf74
Ishi <syi378039@gmail.com>
2024-04-03 04:05:24 +0900

    hook作成

--- includes/render.h
+++ includes/render.h
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 03:18:31 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/03 03:49:55 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/03 04:03:31 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 # include "mlx.h"
 # include "struct.h"
 
-// my_mlx_init.c
+// hook.c
+int		key_press(int keycode, t_data *data);
+int		close_window(t_data *data);
+
+// init.c
 void	my_mlx_init(t_data *data);
 
 // my_mlx_pixel_put.c
--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,36 +6,16 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 03:18:14 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/03 03:54:22 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/03 04:04:54 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void	my_mlx_init(t_data *data)
-{
-	data->mlx = malloc(sizeof(t_mlx));
-	if (!data->mlx)
-		print_error_and_exit("Malloc failed");
-	data->mlx->mlx = mlx_init();
-	data->mlx->win = mlx_new_window(data->mlx->mlx,
-			WINDOW_WIDTH,
-			WINDOW_HEIGHT,
-			"miniRT");
-	if (!data->mlx->win)
-		print_error_and_exit("mlx_new_window failed");
-	data->mlx->img_ptr = mlx_new_image(data->mlx->mlx,
-			WINDOW_WIDTH,
-			WINDOW_HEIGHT);
-	if (!data->mlx->img_ptr)
-		print_error_and_exit("mlx_new_image failed");
-	data->mlx->addr = mlx_get_data_addr(data->mlx->img_ptr,
-			&data->mlx->bits_per_pixel,
-			&data->mlx->line_length,
-			&data->mlx->endian);
-}
-
 void	render(t_data *data)
 {
 	my_mlx_init(data);
+	mlx_hook(data->mlx->win, 2, 1L << 0, &key_press, data);
+	mlx_hook(data->mlx->win, 17, 1L << 17, &close_window, data);
+	mlx_loop(data->mlx->mlx);
 }
