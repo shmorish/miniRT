f0dcdd5f2f076231467a41f7eb0bd62e49a48448
Ishi <syi378039@gmail.com>
2024-04-02 09:04:17 +0900

    intの方使わなかった。

--- srcs/parser/is_in_range.c
+++ srcs/parser/is_in_range.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 04:17:39 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/02 04:44:16 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/02 09:03:56 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	is_in_range_double(double value, double min, double max)
 	return (min <= value && value <= max);
 }
 
-bool	is_in_range_int(int value, int min, int max)
-{
-	return (min <= value && value <= max);
-}
+// bool	is_in_range_int(int value, int min, int max)
+// {
+// 	return (min <= value && value <= max);
+// }
