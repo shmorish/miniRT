f30d23582c6f68314604767ae89a0518806438cf
Ishi <syi378039@gmail.com>
2024-04-03 04:12:28 +0900

    my_mlx_pixel_put

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,15 +6,36 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 03:18:14 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/03 04:04:54 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/03 04:10:31 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+static int	draw(t_data *data)
+{
+	int x;
+	int y;
+
+	y = 0;
+	while (y < WINDOW_HEIGHT)
+	{
+		x = 0;
+		while (x < WINDOW_WIDTH)
+		{
+			my_mlx_pixel_put(data->mlx, x, y, 0x00FF0000);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(data->mlx->mlx, data->mlx->win, data->mlx->img_ptr, 0, 0);
+	return (0);
+}
+
 void	render(t_data *data)
 {
 	my_mlx_init(data);
+	mlx_loop_hook(data->mlx->mlx, &draw, data);
 	mlx_hook(data->mlx->win, 2, 1L << 0, &key_press, data);
 	mlx_hook(data->mlx->win, 17, 1L << 17, &close_window, data);
 	mlx_loop(data->mlx->mlx);
