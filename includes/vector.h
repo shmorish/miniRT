d7f81618dda5e4fad130e47d05cb94613f797c2d
Ishi <syi378039@gmail.com>
2024-04-05 02:38:01 +0900

    内積 外積

--- includes/vector.h
+++ includes/vector.h
@@ -6,20 +6,26 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 02:29:27 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/05 02:30:28 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/05 02:36:52 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_H
 # define VECTOR_H
 
-#include "struct.h"
+# include "struct.h"
 
 // additon.c
 t_vector	addition(t_vector a, t_vector b);
 
+// cross_product.c
+t_vector	cross_product(t_vector a, t_vector b);
+
 // division.c
 t_vector	division(t_vector a, t_vector b);
+
+// dot_product.c
+double		dot_product(t_vector a, t_vector b);
 
 // multiplication.c
 t_vector	multiplication(t_vector a, t_vector b);
---
+++ srcs/utils/vector/cross_product.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cross_product.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 02:30:43 by tsishika          #+#    #+#             */
+/*   Updated: 2024/04/05 02:35:43 by tsishika         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+t_vector	cross_product(t_vector a, t_vector b)
+{
+	t_vector	result;
+
+	result.x = a.y * b.z - a.z * b.y;
+	result.y = a.z * b.x - a.x * b.z;
+	result.z = a.x * b.y - a.y * b.x;
+	return (result);
+}
