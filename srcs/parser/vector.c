01b33905b4f46454ae348a3e6379384460df6bcf
Ishi-eenn <syi378039@gmail.com>
2024-04-15 15:36:32 +0900

    fin

--- srcs/parser/vector.c
+++ srcs/parser/vector.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 03:59:40 by tsishika          #+#    #+#             */
-/*   Updated: 2024/04/02 05:15:57 by tsishika         ###   ########.fr       */
+/*   Updated: 2024/04/15 15:18:12 by tsishika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,7 @@ t_vector	validate_and_parse_vector_range(char *str, double min, double max)
 		|| !is_in_range_double(vector.y, min, max)
 		|| !is_in_range_double(vector.z, min, max))
 		print_error_and_exit("Invalid range of vector");
+	if (vector.x == 0 && vector.y == 0 && vector.z == 0)
+		print_error_and_exit("Invalid vector");
 	return (vector);
 }
